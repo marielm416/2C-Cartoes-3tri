e081c8d14436d87dd6302c78829269a6f3e152ee
marielm416 <108376745+marielm416@users.noreply.github.com>
2024-11-06 19:35:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Matemática II Anonio Dorigon',
+    'Quem sou eu?',
+    'Mariel Martins de Pitanga'
+)
+
+criaCartao(
+    'Matemática II Anonio Dorigon',
+    'Meu trabalkho é?',
+    'Dar aulas para jovens do Ensino Médio'
+)
+
+criaCartao(
+    'Matemática II Anonio Dorigon',
+    'O que eu mais gosto no meu trabalho?',
+    'pEGAR NO PÉ DOS CASAIS NAMORADORES'
+)
+
+criaCartao(
+    'Matemática II Anonio Dorigon',
+    'Nos finais de semana prefiro?',
+    'Ficar com a família'
+)
+
+criaCartao(
+    'Matemática II Anonio Dorigon',
+    'O palmeiras tem mundial?',
+    'Depende do entrevistado'
+)
